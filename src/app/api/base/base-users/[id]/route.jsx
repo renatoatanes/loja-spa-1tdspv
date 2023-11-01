547283f03185d264b6ffd25bc03a33ae96119ca1
renatoatanes <rm552029@fiap.com.br>
2023-10-31 22:40:41 -0300

    imagens add

--- src/app/api/base/base-users/[id]/route.jsx
+++ src/app/api/base/base-users/[id]/route.jsx
@@ -1,7 +1,20 @@
 import {promises as fs} from 'fs';
 import { NextResponse } from 'next/server';
 
+
+
 export async function GET(request, {params}) {
-    const file  = await fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
-    return NextResponse.json(JSON.parse(file)); 
+    
+    const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
+
+    const id = params.id;
+
+    const usuarios =  await JSON.parse(file);
+    
+    if(id > 0 && id <= usuarios.usuarios.length){
+        return  NextResponse.json(usuarios.usuarios.find((user)=> user.id == id));
+    }else{
+        return id == 0 ? NextResponse.json(usuarios.usuarios) : NextResponse.redirect("http://localhost:3000/error")
+    }
 }
+
